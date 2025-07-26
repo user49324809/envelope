1e06fa380efdb87d9ce4a1e7049830043b29dbbb
Наталья <inna6903zaharova@yandex.ru>
2025-07-26 19:32:29 +0300

    Updates

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { Envelope } from './envelope/envelope';
-
-export const routes: Routes = [
-  { path: '', component: Envelope }
-];
--- src/app/models/product.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Product {
-  id: number;
-  name: string;
-  image: string;
-  price: number;
-  quantity: number;
-}
